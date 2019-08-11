026efaf0584f77fae5e8e1ad70922a77a38677db
jp-hong <jphong0603@gmail.com>
2019-08-12 03:29:51 +0900

    add argv handler

--- code/crawl.py
+++ code/crawl.py
@@ -1,11 +1,37 @@
 
 
-from pkgs.phisherman import *
+from pkgs.phisherman import Phisherman
+import sys
+
+
+def process_args():
+
+    try:
+        start = int(sys.argv[1])
+        end = int(sys.argv[2])
+    except:
+        print("Warning! No agruments entered. Default start and end pages will be used.")
+        start = 0
+        end = 9
+
+    try:
+        if sys.argv[3] == "-mpdisable":
+            mp = False
+            print("Warning! Multiprocessing disabled")
+    except:
+        mp = True
+
+    return start, end, mp
+
+
+def crawl():
+    pass
+
 
 # test function
 def test():
-    p = Phisherman(0, 10)
-    print(p.start, p.end)
+    p = Phisherman(0, 1)
+    print(p.start, p.end, p.njobs)
 
 
 test()
