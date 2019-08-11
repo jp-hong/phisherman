7c1bb47bd1126bd464ee718876a643f4011bfd05
jp-hong <jphong0603@gmail.com>
2019-08-12 02:48:51 +0900

    refactored into Phisherman class

--- code/phisherman.py
+++ code/phisherman.py
@@ -4,35 +4,33 @@ import requests
 from bs4 import BeautifulSoup as bs
 
 
-def make_page_url(page):
-    return "https://www.phishtank.com/phish_search.php?page={}\
-        &active=y&valid=y&Search=Search".format(page)
+class Phisherman:
+
+    def __init__(self):
+        pass
 
 
-def make_detail_page_url(id):
-    return "https://www.phishtank.com/phish_detail.php?phish_id={}".format(id)
+    def __make_page_url(self, page):
+        return "https://www.phishtank.com/phish_search.php?page={}\
+            &active=y&valid=y&Search=Search".format(page)
 
 
-def get_ids(page):
-    response = requests.get(make_page_url(page))
-    soup = bs(response.content, "html.parser")
-    elements = soup.select(".value:first-child > a")
-    ids = [element.text for element in elements]
-    return ids
+    def __make_detail_page_url(self, id):
+        return "https://www.phishtank.com/phish_detail.php?phish_id={}".format(id)
 
 
-def get_data(id):
-    response = requests.get(make_detail_page_url(id))
-    soup = bs(response.content, "html.parser")
-    phish_url = soup.select_one(".padded > div:nth-child(4) > \
-        span:nth-child(1) > b:nth-child(1)").text
-    return phish_url
+    def __get_ids(self, page):
+        response = requests.get(self.__make_page_url(page))
+        soup = bs(response.content, "html.parser")
+        elements = soup.select(".value:first-child > a")
+        ids = [element.text for element in elements]
+        return ids
 
 
-def test():
-    ids = get_ids(0)
-    print(ids)
-
-
-test()
+    def __get_data(self, id):
+        response = requests.get(self.__make_detail_page_url(id))
+        soup = bs(response.content, "html.parser")
+        phish_url = soup.select_one(".padded > div:nth-child(4) > \
+            span:nth-child(1) > b:nth-child(1)").text
+        return phish_url
 
