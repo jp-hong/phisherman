2428675e45251c107649ee199009c13b92e975a8
jp-hong <jphong0603@gmail.com>
2019-08-12 01:03:27 +0900

    update make_url methods

--- code/phisherman.py
+++ code/phisherman.py
@@ -4,12 +4,17 @@ import requests
 from bs4 import BeautifulSoup as bs
 
 
-def make_page_url(id):
+def make_page_url(page):
+    return "https://www.phishtank.com/phish_search.php?page={}\
+        &active=y&valid=y&Search=Search".format(page)
+
+
+def make_detail_page_url(id):
     return "https://www.phishtank.com/phish_detail.php?phish_id={}".format(id)
 
 
 def get_data(id):
-    response = requests.get(make_page_url(id))
+    response = requests.get(make_detail_page_url(id))
     soup = bs(response.content, "html.parser")
     phish_url = soup.select_one(".padded > div:nth-child(4) > \
         span:nth-child(1) > b:nth-child(1)").text
