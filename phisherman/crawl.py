499268023733286da586c18f00cc68afe57a7464
jp-hong <jphong0603@gmail.com>
2019-08-16 20:14:37 +0900

    functionally complete

--- phisherman/crawl.py
+++ phisherman/crawl.py
@@ -5,15 +5,10 @@ from pkgs.util import process_args, save_csv
 
 
 def crawl():
-    pass
-
-
-# test function
-def test():
     start, end = process_args()
     phisherman = Phisherman(start, end)
     data = phisherman.crawl()
     save_csv(data, "new.csv")
-    
 
-test()
+
+crawl()
