0af058d44dbc017b7d688c766092a2384a2bbff9
jp-hong <jphong0603@gmail.com>
2019-08-16 19:09:41 +0900

    edit logging message

--- code/pkgs/phisherman.py
+++ code/pkgs/phisherman.py
@@ -40,7 +40,7 @@ class Phisherman:
 
     def __get_ids(self, page):
 
-        print("Gathering links from page {}...".format(page), end="")
+        print("Gathering links from page [{}]...".format(page), end="")
         response = requests.get(self.__make_page_url(page))
 
         if response.status_code == 200:
@@ -56,7 +56,7 @@ class Phisherman:
 
     def __get_data(self, url_id):
 
-        print("Gathering data for url {}...".format(url_id), end="")
+        print("Gathering data for url [id={}]...".format(url_id), end="")
         response = requests.get(self.__make_detail_page_url(url_id))
 
         if response.status_code == 200:
